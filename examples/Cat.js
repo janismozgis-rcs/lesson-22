bd1e1a8137f0352de6eb1712a2f30d9ea977d541
janismozgis-rcs <35929587+janismozgis-rcs@users.noreply.github.com>
2020-12-07 19:45:17 +0200

    Update Cat.js

--- examples/Cat.js
+++ examples/Cat.js
@@ -3,7 +3,7 @@ import Animal from './Animal.js';
 class Cat extends Animal {
 
     purr() {
-        console.log(this.name + ' is purring');
+        console.log(this.name + ' is purring 222');
     }
 }
 
